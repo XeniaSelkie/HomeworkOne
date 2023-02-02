9a2747d356d15ca2568dee52550a31e5dfbbd609
Sana <mypostoffice94@gmail.com>
2023-02-02 17:41:33 +0300

    Add Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите первое число");
+int.TryParse(Console.ReadLine(), out int FirstNumber);
+Console.WriteLine("Введите второе число");
+int.TryParse(Console.ReadLine(), out int SecondNumber);
+
+if (FirstNumber > SecondNumber)
+{
+    Console.WriteLine($"{FirstNumber} - большее число, {SecondNumber} - меньшее число");
+}
+    else
+{
+    Console.WriteLine($"{SecondNumber} - большее число, {FirstNumber} - меньшее число");
+}
