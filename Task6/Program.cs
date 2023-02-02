caa316d351bade5da1f8ec979b11759a2ec4c35d
Sana <mypostoffice94@gmail.com>
2023-02-02 18:15:14 +0300

    Add Task6

---
+++ Task6/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите число");
+int.TryParse(Console.ReadLine(), out int JustNumber);
+int TestForEven = JustNumber % 2;
+if (TestForEven == 0)
+{
+    Console.WriteLine($"Число {JustNumber} - чётное");
+}
+    else
+{
+    Console.WriteLine($"Число {JustNumber} - нечётное");
+}
