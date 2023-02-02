c2e13087fa39f6c85be682851f7212fcffe2c0a0
Sana <mypostoffice94@gmail.com>
2023-02-02 19:30:34 +0300

    Add Task8

---
+++ Task8/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.Write("Введите число");
+int.TryParse(Console.ReadLine(), out int NumberN);
+int TestForEven = 0;
+Console.Write("Чётные числа от 1 до введённого числа: ");
+if (NumberN == 1) 
+{
+ Console.Write("таких не существует.");   
+}
+else if (NumberN < 1)
+     {
+         while (NumberN < 1)
+         {
+         if (TestForEven == NumberN % 2)
+         {Console.Write($"{NumberN}, ");
+         }
+         NumberN = NumberN + 1;
+         }  
+     }
+    else
+    while (NumberN > 1)
+    {
+    if (TestForEven == NumberN % 2)
+    {Console.Write($"{NumberN}, ");
+    }
+    NumberN = NumberN - 1;
+    }
