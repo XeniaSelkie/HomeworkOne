27f8d6e7cf54357df43ab11b84f1bcdddfc51dc5
Sana <mypostoffice94@gmail.com>
2023-02-02 17:57:07 +0300

    Add Task4

---
+++ Task4/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Write("Введите первое число");
+int.TryParse(Console.ReadLine(), out int FirstNumber);
+Console.WriteLine("Введите второе число");
+int.TryParse(Console.ReadLine(), out int SecondNumber);
+Console.WriteLine("Введите третье число");
+int.TryParse(Console.ReadLine(), out int ThirdNumber);
+
+if (FirstNumber > SecondNumber && FirstNumber > ThirdNumber)
+{
+Console.WriteLine($"{FirstNumber} - максимальное число из трёх введённых");
+}
+else if (SecondNumber > FirstNumber && SecondNumber > ThirdNumber)
+    {
+    Console.WriteLine($"{SecondNumber} - максимальное число из трёх введённых");
+    }
+    else 
+    {
+    Console.WriteLine($"{ThirdNumber} - максимальное число из трёх введённых");
+    }
+
+
+  
